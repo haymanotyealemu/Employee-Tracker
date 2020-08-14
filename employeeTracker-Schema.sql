fba68506ed6318ed0d57eb5474b2f9c24326de4c
Haymanot W <haymanotalemu21@gmail.com>
2020-08-14 11:45:22 -0400

    creating a foreign key in schema sql

--- employeeTracker-Schema.sql
+++ employeeTracker-Schema.sql
@@ -2,23 +2,23 @@ DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE database employee_trackerDB;
 
 USE employee_trackerDB;
-CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10,2),
-  PRIMARY KEY (id)
-  FOREIGN KEY (department_id) REFERENCES departments(id)
+  salary DECIMAL,
+  department_id INT,
+  foreign key (department_id) references department(id)
 );
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES role(id),
+  role_id INT,
+  manager_id INT,
+  foreign key (role_id) references role(id),
+  foreign key (manager_id) references role(id)
 );
